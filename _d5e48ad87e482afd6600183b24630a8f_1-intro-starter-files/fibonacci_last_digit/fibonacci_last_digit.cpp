a89f2ff74808f7a49999d8664ec776a80d472173
Abhinav Mishra <mabhi684@gmail.com>
2016-06-07 18:22:24 +0530

    last digit of a large fib in c++

--- _d5e48ad87e482afd6600183b24630a8f_1-intro-starter-files/fibonacci_last_digit/fibonacci_last_digit.cpp
+++ _d5e48ad87e482afd6600183b24630a8f_1-intro-starter-files/fibonacci_last_digit/fibonacci_last_digit.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 
-int get_fibonacci_last_digit(int n) {
-  //write your code here
-  return 0;
+int calc_fib(int n) {
+	int arr[n];
+	arr[0] = 0;
+	arr[1] = 1;
+    if (n <= 1)
+        return n;
+
+    for(int i = 2;i<=n;i++)
+    	arr[i] = (arr[i-1]+arr[i-2])%10;
+    return arr[n];
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  int c = get_fibonacci_last_digit(n);
-  std::cout << c << '\n';
+    int n = 0;
+    std::cin >> n;
+
+    std::cout << calc_fib(n) << '\n';
+    return 0;
 }
