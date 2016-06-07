fc4db0695d7c20b656649943e28588e825f398ce
Abhinav Mishra <mabhi684@gmail.com>
2016-06-07 18:07:58 +0530

    nth fibonaci number

--- _d5e48ad87e482afd6600183b24630a8f_1-intro-starter-files/fibonacci/fib.cc
+++ _d5e48ad87e482afd6600183b24630a8f_1-intro-starter-files/fibonacci/fib.cc
@@ -1,10 +1,15 @@
 #include <iostream>
 
 int calc_fib(int n) {
+	int arr[n];
+	arr[0] = 0;
+	arr[1] = 1;
     if (n <= 1)
         return n;
 
-    return calc_fib(n - 1) + calc_fib(n - 2);
+    for(int i = 2;i<=n;i++)
+    	arr[i] = arr[i-1]+arr[i-2];
+    return arr[n];
 }
 
 int main() {
