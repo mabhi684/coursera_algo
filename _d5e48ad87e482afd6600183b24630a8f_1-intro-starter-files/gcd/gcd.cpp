886808185ec803f2cd3dfa5a500ba74aa5d317ea
Abhinav Mishra <mabhi684@gmail.com>
2016-06-09 11:45:12 +0530

    gcd using eculidian algo

--- _d5e48ad87e482afd6600183b24630a8f_1-intro-starter-files/gcd/gcd.cpp
+++ _d5e48ad87e482afd6600183b24630a8f_1-intro-starter-files/gcd/gcd.cpp
@@ -2,20 +2,24 @@
 
 int gcd(int a, int b) {
   //write your code here
-  int current_gcd = 1;
-  for (int d = 2; d <= a && d <= b; d++) {
-    if (a % d == 0 && b % d == 0) {
-      if (d > current_gcd) {
-        current_gcd = d;
-      }
-    }
+  if(b==0)
+    return a;
+  else
+  {
+    return gcd(b,a%b);
   }
-  return current_gcd;
 }
 
 int main() {
   int a, b;
   std::cin >> a >> b;
+  if(a<b)
+  {
+    int temp = 0;
+    temp = a;
+    a = b;
+    b = temp;
+  }
   std::cout << gcd(a, b) << std::endl;
   return 0;
 }
