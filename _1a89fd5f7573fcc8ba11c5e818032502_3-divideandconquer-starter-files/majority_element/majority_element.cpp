cfd18d219374d7d1c0a5ce0cc1bf3fb5ebb0e7d6
Abhinav Mishra <mabhi684@gmail.com>
2016-07-03 12:17:21 +0530

    completed majority problem in DnC

--- _1a89fd5f7573fcc8ba11c5e818032502_3-divideandconquer-starter-files/majority_element/majority_element.cpp
+++ _1a89fd5f7573fcc8ba11c5e818032502_3-divideandconquer-starter-files/majority_element/majority_element.cpp
@@ -1,22 +1,65 @@
-#include <algorithm>
 #include <iostream>
+#include <cassert>
 #include <vector>
 
-using std::vector;
-
-int get_majority_element(vector<int> &a, int left, int right) {
-  if (left == right) return -1;
-  if (left + 1 == right) return a[left];
-  //write your code here
-  return -1;
+# define bool long long
+  
+long long findCandidate(long long *, long long);
+bool isMajority(long long *, long long, long long);
+  
+/* Function to print Majority Element */
+void printMajority(long long a[], long long size)
+{
+  /* Find the candidate for Majority*/
+  long long cand = findCandidate(a, size);
+  
+  /* Print the candidate if it is Majority*/
+  if(isMajority(a, size, cand))
+    printf("1");
+  else
+    printf("0");
 }
-
-int main() {
-  int n;
+  
+/* Function to find the candidate for Majority */
+long long findCandidate(long long a[], long long size)
+{
+    long long maj_index = 0, count = 1;
+    long long i;
+    for(i = 1; i < size; i++)
+    {
+        if(a[maj_index] == a[i])
+            count++;
+        else
+            count--;
+        if(count == 0)
+        {
+            maj_index = i;
+            count = 1;
+        }
+    }
+    return a[maj_index];
+}
+  
+/* Function to check if the candidate occurs more than n/2 times */
+bool isMajority(long long a[], long long size, long long cand)
+{
+    long long i, count = 0;
+    for (i = 0; i < size; i++)
+      if(a[i] == cand)
+         count++;
+    if (count > size/2)
+       return 1;
+    else
+       return 0;
+}
+  
+ int main() {
+  long long n;
   std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); ++i) {
+  long long a[n];
+  for (size_t i = 0; i < n; ++i) {
     std::cin >> a[i];
   }
-  std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+  long long size = (sizeof(a))/sizeof(a[0]);
+    printMajority(a, size);
 }
